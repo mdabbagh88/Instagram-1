dd47c8ce99b04b6c37e7e6d0a712feef5b5b7052
ericcgu <eric.changning.gu@gmail.com>
2014-12-31 10:04:51 -0500

    Add Instagram Before Infinite Scroll

--- Blocstagram/BLCDataSource.m
+++ Blocstagram/BLCDataSource.m
@@ -17,6 +17,7 @@
 @property (nonatomic, assign) BOOL isRefreshing;
 @property (nonatomic, assign) BOOL isLoadingOlderItems;
 @property (nonatomic, strong) NSString *accessToken;
+@property (nonatomic, strong) NSMutableArray *mediaItems;
 
 @end
 
@@ -50,7 +51,7 @@
   [[NSNotificationCenter defaultCenter] addObserverForName:BLCLoginViewControllerDidGetAccessTokenNotification object:nil queue:nil usingBlock:^(NSNotification *note)
   {
     self.accessToken = note.object;
-    [self populateDataWithParameters:nil];
+    [self populateDataWithParameters:nil completionHandler:nil];
   }];
 }
 
@@ -59,54 +60,114 @@
   return @"12e2324311df4afa839eccdc6c2ef559";
 }
 
-- ( void ) populateDataWithParameters:( NSDictionary * )parameters
-{
-  if ( self.accessToken )
-  {
-    // only try to get the data if there's an access token
-         
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^
-    {
-      // do the network request in the background, so the UI doesn't lock up
-             
-      NSMutableString *urlString = [NSMutableString stringWithFormat:@"https://api.instagram.com/v1/users/self/feed?access_token=%@", self.accessToken];
-             
-      for ( NSString *parameterName in parameters )
-      {
-        // for example, if dictionary contains {count: 50}, append `&count=50` to the URL
-        [urlString appendFormat:@"&%@=%@", parameterName, parameters[parameterName]];
-      }
-             
-      NSURL *url = [NSURL URLWithString:urlString];
-             
-      if ( url )
-      {
-        NSURLRequest *request = [NSURLRequest requestWithURL:url];
-     
-        NSURLResponse *response;
-        NSError *webError;
-        NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&webError];
-     
-        NSError *jsonError;
-        NSDictionary *feedDictionary = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:&jsonError];
-  
-        if ( feedDictionary )
-        {
-          dispatch_async( dispatch_get_main_queue(), ^
-          {
-          // done networking, go back on the main thread
-          [self parseDataFromFeedDictionary:feedDictionary fromRequestWithParameters:parameters];
-          });
-        }
-      }
-    });
-  }
-}
+ - (void) populateDataWithParameters:(NSDictionary *)parameters completionHandler:(BLCNewItemCompletionBlock)completionHandler {
+     if (self.accessToken) {
+         // only try to get the data if there's an access token
+
+         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+             // do the network request in the background, so the UI doesn't lock up
+
+             NSMutableString *urlString = [NSMutableString stringWithFormat:@"https://api.instagram.com/v1/users/self/feed?access_token=%@", self.accessToken];
+
+             for (NSString *parameterName in parameters) {
+                 // for example, if dictionary contains {count: 50}, append `&count=50` to the URL
+                 [urlString appendFormat:@"&%@=%@", parameterName, parameters[parameterName]];
+             }
+
+             NSURL *url = [NSURL URLWithString:urlString];
+
+             if (url) {
+                 NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+                 NSURLResponse *response;
+                 NSError *webError;
+                 NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&webError];
+
+                 if (responseData) {
+                     NSError *jsonError;
+                     NSDictionary *feedDictionary = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:&jsonError];
+ 
+                     if (feedDictionary) {
+                         dispatch_async(dispatch_get_main_queue(), ^{
+                             // done networking, go back on the main thread
+                             [self parseDataFromFeedDictionary:feedDictionary fromRequestWithParameters:parameters];
+                             if (completionHandler) {
+                                 completionHandler(nil);
+                             }
+                         });
+                     } else if (completionHandler) {
+                         dispatch_async(dispatch_get_main_queue(), ^{
+                             completionHandler(jsonError);
+                         });
+                     }
+                 } else if (completionHandler) {
+                     dispatch_async(dispatch_get_main_queue(), ^{
+                         completionHandler(webError);
+                     });
+                 }
+             }
+         });
+     }
+ }
  
 - ( void ) parseDataFromFeedDictionary:( NSDictionary * ) feedDictionary fromRequestWithParameters:( NSDictionary * )parameters
 {
-  NSLog(@"%@", feedDictionary);
+   NSArray *mediaArray = feedDictionary[@"data"];
+ 
+     NSMutableArray *tmpMediaItems = [NSMutableArray array];
+ 
+     for (NSDictionary *mediaDictionary in mediaArray) {
+         BLCMedia *mediaItem = [[BLCMedia alloc] initWithDictionary:mediaDictionary];
+ 
+         if (mediaItem) {
+             [tmpMediaItems addObject:mediaItem];
+             [self downloadImageForMediaItem:mediaItem];
+         }
+     }
+ 
+     NSMutableArray *mutableArrayWithKVO = [self mutableArrayValueForKey:@"mediaItems"];
+ 
+     if (parameters[@"min_id"]) {
+         // This was a pull-to-refresh request
+ 
+         NSRange rangeOfIndexes = NSMakeRange(0, tmpMediaItems.count);
+         NSIndexSet *indexSetOfNewObjects = [NSIndexSet indexSetWithIndexesInRange:rangeOfIndexes];
+ 
+         [mutableArrayWithKVO insertObjects:tmpMediaItems atIndexes:indexSetOfNewObjects];
+     } else {
+         [self willChangeValueForKey:@"mediaItems"];
+         self.mediaItems = tmpMediaItems;
+         [self didChangeValueForKey:@"mediaItems"];
+     }
 }
+
+ - (void) downloadImageForMediaItem:(BLCMedia *)mediaItem {
+     if (mediaItem.mediaURL && !mediaItem.image) {
+         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+             NSURLRequest *request = [NSURLRequest requestWithURL:mediaItem.mediaURL];
+ 
+             NSURLResponse *response;
+             NSError *error;
+             NSData *imageData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+ 
+             if (imageData) {
+                 UIImage *image = [UIImage imageWithData:imageData];
+ 
+                 if (image) {
+                     mediaItem.image = image;
+ 
+                     dispatch_async(dispatch_get_main_queue(), ^{
+                         NSMutableArray *mutableArrayWithKVO = [self mutableArrayValueForKey:@"mediaItems"];
+                         NSUInteger index = [mutableArrayWithKVO indexOfObject:mediaItem];
+                         [mutableArrayWithKVO replaceObjectAtIndex:index withObject:mediaItem];
+                     });
+                 }
+             } else {
+                 NSLog(@"Error downloading image: %@", error);
+             }
+         });
+     }
+  }
 
 #pragma mark - Key/Value Observing
 
@@ -151,13 +212,17 @@
   if ( self.isRefreshing == NO )
   {
      self.isRefreshing = YES;
+    
+     NSString *minID = [[self.mediaItems firstObject] idNumber];
+         NSDictionary *parameters = @{@"min_id": minID};
 
-     self.isRefreshing = NO;
-
-     if ( completionHandler )
-     {
-       completionHandler(nil);
-     }
+     [self populateDataWithParameters:parameters completionHandler:^(NSError *error) {
+             self.isRefreshing = NO;
+ 
+             if (completionHandler) {
+                 completionHandler(error);
+             }
+         }];
   }
 }
 
--- Blocstagram/BLCMediaTableViewCell.m
+++ Blocstagram/BLCMediaTableViewCell.m
@@ -170,7 +170,13 @@ static NSParagraphStyle *paragraphStyle;
   self.mediaImageView.image = _mediaItem.image;
   self.usernameAndCaptionLabel.attributedText = [self usernameAndCaptionString];
   self.commentLabel.attributedText = [self commentString];
-  self.imageHeightConstraint.constant = self.mediaItem.image.size.height / self.mediaItem.image.size.width * CGRectGetWidth(self.contentView.bounds);
+  if (_mediaItem.image) {
+          self.imageHeightConstraint.constant = self.mediaItem.image.size.height / self.mediaItem.image.size.width * CGRectGetWidth(self.contentView.bounds);
+      } else {
+          self.imageHeightConstraint.constant = 0;
+      }
+
+
 }
 
 -( BLCMedia * )mediaItem
@@ -191,16 +197,15 @@ static NSParagraphStyle *paragraphStyle;
       return CGRectGetMaxY(layoutCell.commentLabel.frame);
 }
 
-- ( void )awakeFromNib
-{
-  // Initialization code
-}
-
 - ( void )setSelected:( BOOL )selected animated:( BOOL )animated
 {
-  [super setSelected:selected animated:animated];
+  [super setSelected:NO animated:animated];
 
   // Configure the view for the selected state
 }
+
+ - (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated {
+     [super setHighlighted:NO animated:animated];
+ }
 
 @end
